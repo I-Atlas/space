561a33c617875557945549c61130673dace0f9d9
I-Atlas <54947380+I-Atlas@users.noreply.github.com>
2021-11-26 13:35:10 +0300

    [SPACE-2] fix: change mydoc logo, delete unused home.module.css

--- data/projects.ts
+++ data/projects.ts
@@ -21,7 +21,7 @@ export const projects: Project[] = [
     id: 3,
     title: "MyDoc",
     link: "",
-    logo: "/assets/images/projects/mydoc.png",
+    logo: "/assets/images/projects/mydoc.svg",
     blurHash: "AfMuFp%J~Ss.",
     desc: `A mobile application that allows you to choose YOUR doctor from the list of great specialists, book a date and time, store documents, analysis, receive prescriptions for medications and view all your activity. We made the design as simple as possible, but nevertheless friendly, because patients often feel anxious before going to the doctor and our application should help them to relax and feel in comfort at every step.`,
     technologies: ["typescript", "react-native", "node.js"],
